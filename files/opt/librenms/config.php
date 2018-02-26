e44052a5840f9ddfa3b6e4d12b86966ddd1ea6cb
jari.schaefer <jari.schaefer@gmail.com>
2018-02-26 17:29:34 +0100

    configurable rrdcached socket (supporting tcp sockets)

--- files/opt/librenms/config.php
+++ files/opt/librenms/config.php
@@ -19,7 +19,7 @@ $config['update'] = 0;
 $config['nagios_plugins']   = "/usr/lib/nagios/plugins";
 
 $config['rrdtool_version'] = '1.5.5';
-$config['rrdcached'] = "unix:/var/run/rrdcached/rrdcached.sock";
+$config['rrdcached'] = getenv('RRDCACHED_CONNECT') ?: "unix:/var/run/rrdcached/rrdcached.sock";
 
 $config['memcached']['enable'] = filter_var(getenv('MEMCACHED_ENABLE'), FILTER_VALIDATE_BOOLEAN);
 $config['memcached']['host'] = getenv('MEMCACHED_HOST');
